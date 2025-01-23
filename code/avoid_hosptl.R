c91324039eec996ec0ad1e34fb86f92a13a1da03
Shadi Sadie <shadi.sedi@gmail.com>
2025-01-23 18:14:24 -0500

    update to only keep the function for calling in data cleaning master

--- code/avoid_hosptl.R
+++ code/avoid_hosptl.R
@@ -1,27 +1,3 @@
-library(tidyverse)
-library(MEPS)
-
-yearset <- 2011:2019
-condsets <- vector("list", length(yearset))
-fyc_names <- paste0("fyc", 11:19)  # Creates "fyc11", "fyc12", ..., "fyc15"
-fycsets <- mget(fyc_names)
-# Define ICD codes
-icd10_codes <- list(
-    diabetes = "E11",
-    hypertension = "I10",
-    copd = c("J44", "J45"),
-    chf = "I50",
-    pneumonia = "J18",
-    uti = "N39"
-)
-icd9_codes <- list(
-    diabetes = "250",
-    hypertension = c("401", "402", "403"),
-    copd = c("491", "490", "496", "466", "492", "494"),
-    chf = c("428", "398"),
-    pneumonia = c("480", "481", "482", "483", "486"),
-    uti = "599"
-)
 
 # Process Condition Data
 process_condsets <- function(condata, fycdata, icd_col, codes) {
@@ -43,29 +19,5 @@ process_condsets <- function(condata, fycdata, icd_col, codes) {
         left_join(condata) %>%
         mutate(across(starts_with("ip"), ~ replace_na(.x, 0)))
 }
-
-# Loop through years and process
-
-for (i in seq_along(yearset)) {
-    condsets[[i]] <- read_MEPS(year = yearset[i], type = "COND")
-    
-    if (yearset[i] < 2016) {
-        fycsets[[i]] <- process_condsets(condsets[[i]], fycsets[[i]], "ICD9CODX", icd9_codes)
-    } else {
-        fycsets[[i]] <- process_condsets(condsets[[i]], fycsets[[i]], "ICD10CDX", icd10_codes)
-    }
-    assign(paste0('fyc', substring(as.character(yearset[[i]]),3,4)),fycsets[[i]])
-    
-}
-
-# Assign names for better tracking
-# Access aggregated data
-#year<-11:19
-# (i in seq_along(11:19)) {
-#paste0('agfyc', substring(yearset[[i]],2) <- fycsets[[i]]
-
-table(agfyc12$ip_AvdHOSP)
-
-#   yearset[[1]]
 
 
